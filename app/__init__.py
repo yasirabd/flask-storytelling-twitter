4ea586a8fc5b9dd573298214274e9691bacbab77
yasirabd <sir.yasirabd@gmail.com>
2018-03-13 14:47:33 +0700

    result of attractions

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_babel import Babel
 from flask_googlemaps import GoogleMaps
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 bootstrap = Bootstrap(app)
--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,20 @@ from app import app
 from flask import render_template, url_for, flash
 from flask_googlemaps import Map
 from app.forms import SearchPlaceForm, SearchTweets
+from flask_babel import _
+from googleplaces import GooglePlaces
 
-@app.route('/', methods=['GET'])
-@app.route('/index', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form_splace = SearchPlaceForm()
     form_stweets = SearchTweets()
+    attractions = None
+    GOOGLEMAPS_KEY = "AIzaSyCc3VpBAxqVIwkCvQC1ibFGFnqJbXDmxwE"
+    google_places = GooglePlaces(GOOGLEMAPS_KEY)
     if form_splace.validate_on_submit():
-        flash(_('Wait for your result!'))
-    return render_template('index.html', form_splace=form_splace, form_stweets=form_stweets)
+        place_name = form_splace.place_name.data
+        attractions = google_places.text_search(query=place_name+' tourist attractions',
+                                                language='id')
+    return render_template('index.html', form_splace=form_splace,
+                            form_stweets=form_stweets, attractions=attractions)
