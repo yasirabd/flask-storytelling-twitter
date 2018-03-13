709d479302e4ab0b9f02db4d1c9060c69427b871
yasirabd <sir.yasirabd@gmail.com>
2018-03-13 11:36:19 +0700

    maps change language and location only for indonesia

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, HiddenField
 from wtforms.validators import ValidationError, DataRequired
 from flask_babel import _, lazy_gettext as _l
 
-class SearchForm(FlaskForm):
+class SearchPlaceForm(FlaskForm):
     place = StringField(_l('Place'), validators=[DataRequired()])
-    range_distance = StringField(_l('Range distance'), validators=[DataRequired()])
+    place_name = HiddenField(_l('Place name'), validators=[DataRequired()])
+    search_place = SubmitField(_l('Search'))
+
+class SearchTweets(FlaskForm):
+    latitude = HiddenField(_l('Latitude'), validators=[DataRequired()])
+    longitude = HiddenField(_l('Longitude'), validators=[DataRequired()])
+    range_dist = StringField(_l('Range distance'), validators=[DataRequired()])
     days_before = StringField(_l('Days before'), validators=[DataRequired()])
-    search = SubmitField(_l('Search'))
+    search_tweets = SubmitField(_l('Search'))
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from app import app
 from flask import render_template, url_for, flash
 from flask_googlemaps import Map
-from app.forms import SearchForm
+from app.forms import SearchPlaceForm, SearchTweets
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-    form = SearchForm()
-    if form.validate_on_submit():
+    form_splace = SearchPlaceForm()
+    form_stweets = SearchTweets()
+    if form_splace.validate_on_submit():
         flash(_('Wait for your result!'))
-    return render_template('index.html', form=form)
+    return render_template('index.html', form_splace=form_splace, form_stweets=form_stweets)
