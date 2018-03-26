f56c553d7b9458641dcbdd98fe220dab03da1a04
yasirabd <sir.yasirabd@gmail.com>
2018-03-26 22:36:36 +0700

    attraction checkboxes, error display

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, HiddenField
-from wtforms.validators import ValidationError, DataRequired
+from wtforms import StringField, SubmitField, HiddenField, SelectMultipleField, widgets, SelectField, FloatField
+from wtforms.validators import ValidationError, DataRequired, InputRequired
 from flask_babel import _, lazy_gettext as _l
 
 
@@ -12,9 +12,27 @@ class SearchPlaceForm(FlaskForm):
     search_place = SubmitField(_l('Search'))
 
 
-class SearchTweets(FlaskForm):
+class ChoiceObj(object):
+    def __init__(self, name, choices):
+        # this is needed so that BaseForm.process will accept the object for the named form,
+        # and eventually it will end up in SelectMultipleField.process_data and get assigned
+        # to .data
+        setattr(self, name, choices)
+
+
+class SelectMultipleAttractions(SelectMultipleField):
+    def pre_validate(self, form):
+        # Prevent "not a valid choice" error
+        pass
+
+    widget = widgets.TableWidget()
+    option_widget = widgets.CheckboxInput()
+
+
+class SearchTweetsForm(FlaskForm):
     latitude = HiddenField(_l('Latitude'), validators=[DataRequired()])
     longitude = HiddenField(_l('Longitude'), validators=[DataRequired()])
+    multi_attractions = SelectMultipleAttractions('Attractions', choices=[], validators=[InputRequired()])
     range_dist = StringField(_l('Range distance'), validators=[DataRequired()])
     days_before = StringField(_l('Days before'), validators=[DataRequired()])
-    search_tweets = SubmitField(_l('Search'))
+    submit = SubmitField(_l('Crawl Tweets'))
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,23 +1,59 @@
 from app import current_app
-from flask import render_template, url_for, flash
+from flask import render_template, url_for, session, redirect
 from flask_googlemaps import Map
 from flask_babel import _
 from googleplaces import GooglePlaces
 from app.main import bp
-from app.main.forms import SearchPlaceForm, SearchTweets
+from app.main.forms import SearchPlaceForm, SearchTweetsForm, ChoiceObj
+from app._helpers import flash_errors
 
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
     form_splace = SearchPlaceForm()
-    form_stweets = SearchTweets()
+    form_stweets = SearchTweetsForm()
+
+    return render_template('index.html', form_splace=form_splace,
+                            form_stweets=form_stweets)
+
+
+@bp.route('/attractions', methods=['GET', 'POST'])
+def attractions():
+    selectedChoices = ChoiceObj('multi_attractions', session.get('selected'))
+    form_splace = SearchPlaceForm()
+    form_stweets = SearchTweetsForm(obj=selectedChoices)
+
     attractions = None
+    list_attractions = ()
     GOOGLEMAPS_KEY = "AIzaSyCc3VpBAxqVIwkCvQC1ibFGFnqJbXDmxwE"
     google_places = GooglePlaces(GOOGLEMAPS_KEY)
     if form_splace.validate_on_submit():
         place_name = form_splace.place_name.data
         attractions = google_places.text_search(query=place_name+' tourist attractions',
                                                 language='id')
+        if attractions:
+            la = list(list_attractions)
+            for place in attractions.places:
+                la.append(place.name)
+            list_attractions = tuple(la)
+
+        form_stweets.multi_attractions.choices =  [(att, att) for att in list_attractions]
+        form_stweets.latitude.data = form_splace.lat.data
+        form_stweets.longitude.data = form_splace.lng.data
     return render_template('index.html', form_splace=form_splace,
-                            form_stweets=form_stweets, attractions=attractions)
+                            form_stweets=form_stweets)
+
+@bp.route('/crawl', methods=['GET', 'POST'])
+def crawl():
+    selectedChoices = ChoiceObj('attractions', session.get('selected'))
+    form_splace = SearchPlaceForm()
+    form_stweets = SearchTweetsForm(obj=selectedChoices)
+
+    if form_stweets.validate_on_submit():
+        session['selected'] = form_stweets.multi_attractions.data
+        return render_template('result.html', form_stweets=form_stweets, selected=session.get('selected'))
+
+    flash_errors(form_stweets)
+    return render_template('index.html', form_splace=form_splace,
+                            form_stweets=form_stweets, selected=session.get('selected'))
